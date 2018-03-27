d5b51d5ce59679d8d2a5a6fe369d16c7d8f4a8e0
samick17 <boneache@gmail.com>
2018-03-28 00:00:28 +0800

    1. fix setup script for node-abi-version updated

--- setup.js
+++ setup.js
@@ -2,7 +2,7 @@ const NodeABI = require('node-abi-version');
 const fs = require('fs');
 
 module.exports = function() {
-  const abi = NodeABI[process.version];
+  const abi = NodeABI.getABIVersion();
   var srcPath = __dirname+'/dist/Automator'+abi+'.node';
   var destPath = __dirname+'/dist/Automator.node';
   fs.writeFileSync(destPath, fs.readFileSync(srcPath));
