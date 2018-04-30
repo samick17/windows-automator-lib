9756d17928bcaeea60a1426321c9532b2ed9e23c
samick17 <boneache@gmail.com>
2018-04-30 23:20:47 +0800

    1. rebuild all target for ia32 instead of x86 2. update version to 1.4.3

--- build-target.bat
+++ build-target.bat
@@ -2,7 +2,7 @@
 
 call node-gyp configure build --target=%1 --arch=ia32
 echo copy
-call copy .\build\Release\Automator.node .\dist\Automator%2_x86.node
+call copy .\build\Release\Automator.node .\dist\Automator%2_ia32.node
 call node-gyp configure build --target=%1 --arch=x64
 echo copy
 call copy .\build\Release\Automator.node .\dist\Automator%2_x64.node
